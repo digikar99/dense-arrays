198f13094fca9c986086d3dbd6f93b86116681e5
digikar99 <shubhamkarayare@gmail.com>
2022-05-18 11:49:55 +0530

    [add][fix] %subtypep and %intersect-type-p methods

--- src/types.lisp
+++ src/types.lisp
@@ -47,12 +47,35 @@
 (define-array-specialization-type %dense-array dense-array)
 
 #+extensible-compound-types
-(defmethod %subtypep ((n1 (eql '%dense-array)) (n2 (eql 'abstract-array))
-                      t1 t2 &optional env)
-  (declare (ignore n1 n2 t1 env))
-  (if (or (symbolp t2)
-          (and (listp t2)
-               (first t2)
-               (null (cdr t2))))
-      (values t t)
-      (values nil nil)))
+(progn
+  (defmethod %subtypep ((n1 (eql '%dense-array)) (n2 (eql 'abstract-array))
+                        t1 t2 &optional env)
+    (declare (ignore n1 n2 t1 env))
+    (if (or (symbolp t2)
+            (and (listp t2)
+                 (first t2)
+                 (null (cdr t2))))
+        (values t t)
+        (values nil nil)))
+
+  (defmethod %subtypep ((t1 (eql 'array)) (t2 (eql 'simple-array)) type1 type2 &optional env)
+    (declare (ignore t1 t2 type1 type2 env))
+    (values nil t))
+
+  (defmethod %subtypep ((t1 (eql 'simple-array)) (t2 (eql 'array)) type1 type2 &optional env)
+    (declare (ignore t1 t2))
+    (%subtypep 'array 'array
+               `(array ,@(rest (ensure-list type1)))
+               `(array ,@(rest (ensure-list type2))) env))
+
+  (defmethod %intersect-type-p
+    ((t1 (eql 'array)) (t2 (eql 'simple-array)) type1 type2 &optional env)
+    (%intersect-type-p t1 t1 type1 `(array ,@(rest (ensure-list type2))) env))
+  (defmethod %intersect-type-p
+      ((t1 (eql 'simple-array)) (t2 (eql 'array)) type1 type2 &optional env)
+    (%intersect-type-p t2 t2 `(array ,@(rest (ensure-list type1))) type2 env))
+
+  (define-mutually-exclusive-types array cl:array)
+  (define-mutually-exclusive-types array cl:simple-array)
+  (define-mutually-exclusive-types simple-array cl:simple-array)
+  (define-mutually-exclusive-types simple-array cl:array))
