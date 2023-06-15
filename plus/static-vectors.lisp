45f63b08d9c97ce8144c443924bee3859582a80a
digikar99 <shubhamkarayare@gmail.com>
2023-06-15 23:39:37 +0530

    [fix] static-vectors: surround class method definitions in eval-when

--- plus/static-vectors.lisp
+++ plus/static-vectors.lisp
@@ -1,16 +1,17 @@
 (in-package :dense-arrays)
 (in-suite :dense-arrays)
 
-(defclass static-dense-array-class (standard-dense-array-class) ())
+(eval-when (:compile-toplevel :load-toplevel :execute)
+  (defclass static-dense-array-class (standard-dense-array-class) ())
 
-(defclass static-dense-array (dense-array)
-  ()
-  (:metaclass static-dense-array-class))
+  (defclass static-dense-array (dense-array)
+    ()
+    (:metaclass static-dense-array-class))
 
-(defmethod storage-allocator ((class static-dense-array-class))
-  'static-vectors:make-static-vector)
-(defmethod storage-deallocator ((class static-dense-array-class))
-  'static-vectors:free-static-vector)
+  (defmethod storage-allocator ((class static-dense-array-class))
+    'static-vectors:make-static-vector)
+  (defmethod storage-deallocator ((class static-dense-array-class))
+    'static-vectors:free-static-vector))
 
 (define-dense-array-types static-dense-array static-dense-array
   static-array simple-static-array)
