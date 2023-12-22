1b731e36ddcdf73f867daab888ba61d2801195de
digikar <shubhamkarayare@gmail.com>
2023-12-23 02:01:04 +0530

    [fix] use custom-form-type from peltadot

--- plus/lite.lisp
+++ plus/lite.lisp
@@ -169,19 +169,19 @@
            ,@body))
        (declaim (notinline ,name))
 
-       (defmethod cl-form-types:custom-form-type ((op (eql ',name)) args env)
+       (defmethod custom-form-type ((op (eql ',name)) args env)
          (let* ((type-position (position :type args))
                 (elt-type-form (if type-position
                                    (nth (1+ type-position) args)
-                                   (return-from cl-form-types:custom-form-type 'simple-array)))
+                                   (return-from custom-form-type 'simple-array)))
                 (elt-type (if (constantp elt-type-form env)
                               (introspect-environment:constant-form-value elt-type-form env)
-                              (return-from cl-form-types:custom-form-type 'simple-array)))
+                              (return-from custom-form-type 'simple-array)))
                 (dim-form/s (subseq args 0 type-position))
                 (rank (cond ((< 1 (length dim-form/s))
                              (length dim-form/s))
                             (t
-                             (let ((dim-form-type (cl-form-types:form-type (first dim-form/s) env)))
+                             (let ((dim-form-type (form-type (first dim-form/s) env)))
                                (if (type= t dim-form-type)
                                    'cl:*
                                    (multiple-value-bind (subtypep knownp)
